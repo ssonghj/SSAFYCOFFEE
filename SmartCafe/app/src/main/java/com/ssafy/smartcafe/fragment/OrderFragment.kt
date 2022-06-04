3fd88f9280e497a3185f21afe82f482867fa7a56
hyeonjin0103 <hyeonjin0103@o.cnu.ac.kr>
2022-06-04 18:35:26 +0900

    add nested fragment in orderFragment

--- SmartCafe/app/src/main/java/com/ssafy/smartcafe/fragment/OrderFragment.kt
+++ SmartCafe/app/src/main/java/com/ssafy/smartcafe/fragment/OrderFragment.kt
@@ -5,15 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.ssafy.smartcafe.R
+import com.ssafy.smartcafe.databinding.FragmentOrderBinding
 
 class OrderFragment : Fragment() {
-
+    private lateinit var binding:FragmentOrderBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentOrderBinding.inflate(layoutInflater)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false)
+        val navHostFragment = childFragmentManager.findFragmentById(R.id.order_nav_host) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+        return binding.root
     }
 }
