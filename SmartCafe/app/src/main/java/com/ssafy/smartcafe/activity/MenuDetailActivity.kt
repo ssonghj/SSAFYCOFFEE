c1752d0bebc528080018d34812b5dfdf0399f4af
hyeonjin0103 <hyeonjin0103@o.cnu.ac.kr>
2022-06-05 13:35:49 +0900

    add review detail

--- SmartCafe/app/src/main/java/com/ssafy/smartcafe/activity/MenuDetailActivity.kt
+++ SmartCafe/app/src/main/java/com/ssafy/smartcafe/activity/MenuDetailActivity.kt
@@ -1,12 +1,21 @@
 package com.ssafy.smartcafe.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ssafy.smartcafe.R
+import com.ssafy.smartcafe.databinding.ActivityMenuDetailBinding
 
 class MenuDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu_detail)
+        binding = ActivityMenuDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvMoreDetailReview.setOnClickListener{
+            val intent = Intent(this, MenuReviewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
