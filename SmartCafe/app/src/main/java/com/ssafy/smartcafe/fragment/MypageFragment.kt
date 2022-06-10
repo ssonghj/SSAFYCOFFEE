e1265cebcb84098720b03b1f0f02fb0269d71edd
DESKTOP-KVCQHCD\multicampus <yeonbb98@naver.com>
2022-06-11 00:02:12 +0900

    [hy]little change

--- SmartCafe/app/src/main/java/com/ssafy/smartcafe/fragment/MypageFragment.kt
+++ SmartCafe/app/src/main/java/com/ssafy/smartcafe/fragment/MypageFragment.kt
@@ -3,10 +3,13 @@ package com.ssafy.smartcafe.fragment
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelLazy
 import com.ssafy.smartcafe.R
 import com.ssafy.smartcafe.activity.NotificationActivity
 import com.ssafy.smartcafe.activity.SettingActivity
@@ -14,10 +17,18 @@ import com.ssafy.smartcafe.activity.UserRecentOrderListActivity
 import com.ssafy.smartcafe.activity.UserReviewActivity
 import com.ssafy.smartcafe.databinding.FragmentMypageBinding
 import com.ssafy.smartcafe.databinding.FragmentOrderBinding
+import com.ssafy.smartcafe.viewModel.JoinViewModel
+import com.ssafy.smartcafe.viewModel.MypageViewModel
 
+private const val TAG = "MypageFragment"
 class MypageFragment : Fragment() {
     private lateinit var ctx: Context
     private lateinit var binding: FragmentMypageBinding
+    val mainViewModel: MypageViewModel by ViewModelLazy(
+        MypageViewModel::class,
+        { viewModelStore },
+        { defaultViewModelProviderFactory }
+    )
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -51,6 +62,9 @@ class MypageFragment : Fragment() {
             startActivity(intent)
         }
 
+        binding.frameLogout.setOnClickListener {
+            Log.d(TAG, "onCreateView: 로그아웃 버튼 클릭")
+        }
         return binding.root
     }
 
